8348a29ac5dfd912e930e3b608176519833c2f63
Starchy29 <ethanball29@gmail.com>
2025-10-26 20:25:35 -0400

    added shader compilation to batch file

--- ElbranEngine2/build_win32_x64_debug.bat
+++ ElbranEngine2/build_win32_x64_debug.bat
@@ -4,6 +4,13 @@ mkdir builds\windows-x64-debug
 pushd builds\windows-x64-debug
 mkdir assets
 mkdir shaders
-cl /std:c++20 /Zi /DWINDOWS ..\..\cpp\*.cpp kernel32.lib user32.lib DXGI.lib XInput.lib Xinput9_1_0.lib Xaudio2.lib
-(robocopy "$(SolutionDir)Assets" "$(TargetDir)Assets" /mir) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
+cl /std:c++20 /DWINDOWS /Zi ..\..\cpp\*.cpp /FeElbranGame_DEBUG kernel32.lib user32.lib DXGI.lib XInput.lib Xinput9_1_0.lib Xaudio2.lib
+del *.obj
+
+for %%I in (..\..\hlsl\*PS.hlsl ..\..\hlsl\*PP.hlsl) do fxc /T ps_5_1 /Fo shaders\%%~nI.cso %%I
+for %%I in (..\..\hlsl\*VS.hlsl) do fxc /T vs_5_1 /Fo shaders\%%~nI.cso %%I
+for %%I in (..\..\hlsl\*GS.hlsl) do fxc /T gs_5_1 /Fo shaders\%%~nI.cso %%I
+for %%I in (..\..\hlsl\*CS.hlsl) do fxc /T cs_5_1 /Fo shaders\%%~nI.cso %%I
+
+robocopy "..\..\Assets" "assets" /mir
 popd
