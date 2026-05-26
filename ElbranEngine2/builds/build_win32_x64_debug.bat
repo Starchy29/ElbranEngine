d03e966baa8022dc28e281e394c145055d9985b8
Starchy29 <ethanball29@gmail.com>
2026-05-26 09:43:42 -0400

    moved build scripts into builds folder

---
+++ ElbranEngine2/builds/build_win32_x64_debug.bat
@@ -0,0 +1,20 @@
+@echo off
+
+mkdir windows-x64-debug
+pushd windows-x64-debug
+
+del ElbranGame_DEBUG.exe
+cl /std:c++20 /MDd /DWINDOWS /D_DEBUG /DDEBUG /D_UNICODE /D_MBCS /Zi ..\..\cpp\*.cpp /FeElbranGame_DEBUG kernel32.lib user32.lib d3d11.lib dxguid.lib DXGI.lib XInput.lib Xinput9_1_0.lib Xaudio2.lib d3dcompiler.lib
+
+del *.obj
+del *.ilk
+
+popd
+
+for %%I in (..\hlsl\*PS.hlsl) do fxc /T ps_5_0 /Fo ..\assets\%%~nI.cso %%I
+for %%I in (..\hlsl\*VS.hlsl) do fxc /T vs_5_0 /Fo ..\assets\%%~nI.cso %%I
+for %%I in (..\hlsl\*GS.hlsl) do fxc /T gs_5_0 /Fo ..\assets\%%~nI.cso %%I
+for %%I in (..\hlsl\*CS.hlsl) do fxc /T cs_5_0 /Fo ..\assets\%%~nI.cso %%I
+
+ElbranFilePacker.exe ..\assets windows-x64-debug\gameAssets.bin
+del ..\assets\*.cso
---
+++ ElbranEngine2/builds/build_win32_x64_release.bat
@@ -0,0 +1,21 @@
+@echo off
+
+mkdir windows-x64-release
+pushd windows-x64-release
+mkdir Licenses
+del ElbranGame.exe
+
+cl /std:c++20 /MD /DWINDOWS /D_UNICODE /D_MBCS ..\..\cpp\*.cpp /FeElbranGame kernel32.lib user32.lib d3d11.lib dxguid.lib DXGI.lib XInput.lib Xinput9_1_0.lib Xaudio2.lib d3dcompiler.lib
+del *.obj
+
+popd
+
+for %%I in (..\hlsl\*PS.hlsl) do fxc /T ps_5_0 /Fo ..\assets\%%~nI.cso %%I
+for %%I in (..\hlsl\*VS.hlsl) do fxc /T vs_5_0 /Fo ..\assets\%%~nI.cso %%I
+for %%I in (..\hlsl\*GS.hlsl) do fxc /T gs_5_0 /Fo ..\assets\%%~nI.cso %%I
+for %%I in (..\hlsl\*CS.hlsl) do fxc /T cs_5_0 /Fo ..\assets\%%~nI.cso %%I
+
+ElbranFilePacker.exe ..\assets windows-x64-release\gameAssets.bin
+del ..\assets\*.cso
+
+robocopy "..\Licenses" "windows-x64-release\Licenses" /mir
