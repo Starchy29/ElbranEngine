2cd114f1d5f9658cad79c05976853f5bf498a381
Starchy29 <ethanball29@gmail.com>
2026-07-12 10:23:08 -0400

    eliminated compiled engine lib for building release versions

--- ElbranEngine2/builds/compile_engine.bat
+++ ElbranEngine2/builds/compile_engine.bat
@@ -7,12 +7,4 @@ cl /c /std:c++20 /MDd /DWINDOWS /D_DEBUG /DDEBUG /D_UNICODE /D_MBCS /Zi ..\..\cp
 lib *.obj /OUT:elbranEngine_win32_debug.lib 
 del *.obj
 
-set release=true
-if "%~1" == "debug" set release=false
-if %release%==true (
-cl /c /std:c++20 /MD /DWINDOWS /D_UNICODE /D_MBCS ..\..\cpp\engine\*.cpp
-lib *.obj /OUT:elbranEngine_win32_release.lib
-del *.obj
-)
-
 popd
